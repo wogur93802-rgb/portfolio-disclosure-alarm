20ff16a8731fcd86ad23d1b9e18392372c0f5a23
wogur93802-rgb <wogur93802@gmail.com>
2026-07-10 09:18:10 +0900

    Add low price consecutive close radar

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "disclosure-alarm-v9";
+const CACHE = "disclosure-alarm-v10";
 const ASSETS = ["/", "/styles.css", "/app.js", "/manifest.json", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
